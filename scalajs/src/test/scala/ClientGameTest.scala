dd62693e9feef34827fbb797c2b1d826c27ea705
Vincent Munier <vmunier.fr@gmail.com>
2014-07-07 10:27:52 +0100

    Add server reconciliation tests

--- scalajs/src/test/scala/ClientGameTest.scala
+++ scalajs/src/test/scala/ClientGameTest.scala
@@ -11,49 +11,35 @@ object ClientGameTest extends BaseGameTest {
 
   val NbTicks = 5
 
+  trait ClientGameScope {
+    game.clientState = firstLoopState
+    game.serverState = firstLoopState
+    val nextMove = Down
+  }
+
   describe("A snake") {
-    it("should move properly after a client tick") {
-      game.clientState = firstLoopState
+    it("should move properly after a client tick")(new ClientGameScope {
       game.onGameTick
 
       val movedPlayerSnake = getSnakeFromState(game.clientState, playerSnake.snakeId).get
       assert(movedPlayerSnake.head.pos == Position(3, 2))
-    }
+    })
   }
 
   describe("The server reconciliation") {
-    it("should have no effect when the client state matches the server state") {
-      game.clientState = firstLoopState
-      game.serverState = firstLoopState
-
-      val nextMove = Down
-
+    it("should have no effect when the client state matches the server state")(new ClientGameScope {
       val numTickWhenToChangeMove = 4
-      for (numTick <- 1 to NbTicks) {
-        if (numTick == numTickWhenToChangeMove) {
-          game.nextMove = nextMove
-
-        }
-        game.onGameTick
-      }
+      runClientGameWithOneMoveChange(numTickWhenToChangeMove, nextMove)
 
       val savedClientState = game.clientState
       game.onGameLoopNotif(GameLoopNotif(new GameLoopId(numTickWhenToChangeMove), snakeMoves = Set(SnakeMove(playerSnake.snakeId, nextMove))))
       assert(savedClientState == game.clientState)
-    }
+    })
 
-    it("should take place when the client state diverges from the server state") {
-      game.clientState = firstLoopState
-      game.serverState = firstLoopState
 
-      val nextMove = Down
+    it("should take place when the client state diverges from the server state")(new ClientGameScope {
       val numTickWhenToChangeMove = 4
-      for (numTick <- 1 to NbTicks) {
-        if (numTick == numTickWhenToChangeMove) {
-          game.nextMove = nextMove
-        }
-        game.onGameTick
-      }
+      runClientGameWithOneMoveChange(numTickWhenToChangeMove, nextMove)
 
       val savedClientState = game.clientState
       val serverNotifId = new GameLoopId(2)
@@ -64,6 +50,26 @@ object ClientGameTest extends BaseGameTest {
       assert(savedClientState != game.clientState)
       assert(getSnakeFromState(savedClientState, playerSnake.snakeId) == getSnakeFromState(game.clientState, playerSnake.snakeId))
       assert(getSnakeFromState(game.clientState, otherSnake.snakeId).get.head.pos == expectedOtherSnakePos)
+    })
+
+    it("should replace the client state by the server state when serverState.id >= clientState.id")(new ClientGameScope {
+      val numTickWhenToChangeMove = 4
+      runClientGameWithOneMoveChange(numTickWhenToChangeMove, nextMove)
+
+      val savedClientState = game.clientState
+      val serverNotifId = new GameLoopId(NbTicks + 2)
+      game.onGameLoopNotif(GameLoopNotif(serverNotifId, snakeMoves = Set(SnakeMove(otherSnake.snakeId, Down))))
+
+    })
+  }
+
+  def runClientGameWithOneMoveChange(numTickWhenToChangeMove: Int, nextMove: Move) {
+    for (numTick <- 1 to NbTicks) {
+      if (numTick == numTickWhenToChangeMove) {
+        game.nextMove = nextMove
+
+      }
+      game.onGameTick
     }
   }
 }
