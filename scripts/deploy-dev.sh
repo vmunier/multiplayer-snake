bfc07679d8f59ee7bea374409558ab7079c0b8e8
Vincent Munier <vmunier.fr@gmail.com>
2014-08-02 00:20:28 +0100

    deploy-dev.sh: fix sbt path

--- scripts/deploy-dev.sh
+++ scripts/deploy-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 # http://askubuntu.com/questions/402832/how-do-i-allow-only-one-user-to-su-to-another-account
-sudo -u vmunier zsh -c "sbt -Dsbt.log.noformat=true dist"
+sudo -u vmunier zsh -c "/home/vmunier/install/sbt/bin/sbt -Dsbt.log.noformat=true update test dist"
