6f4a9039688be3eedbf76083480f837594740837
Vincent Munier <vmunier.fr@gmail.com>
2014-08-03 01:29:33 +0100

    Update scripts/push-zip-to-deployments.sh

--- scripts/push-zip-to-deployments.sh
+++ scripts/push-zip-to-deployments.sh
@@ -1,12 +1,18 @@
 #!/bin/zsh
 
+deploymentsBranch="multiplayer-snake"
+zipFolder="scalajvm/target/universal"
+
+scriptPath=$(dirname "$0")
+cd $scriptPath/..
+
 /Users/vmunier/install/sbt/bin/sbt -Dsbt.log.noformat=true update test dist
 
-zipArchive=`find . -name "play-game-*zip"`
-
+zipArchive=$(find $zipFolder -name "*.zip")
 mv -f $zipArchive deployments
 
 cd deployments
+git pull origin $deploymentsBranch
 git add -A
 git commit -m "new zip archive"
-git push origin multiplayer-snake
+git push origin $deploymentsBranch
